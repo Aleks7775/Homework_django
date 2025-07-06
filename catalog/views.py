9522553490dab8349dae25a7809e4e21528e4a16
Aleksandr <feat9999@yandex.ru>
2025-07-06 21:52:49 +0400

    commit updates

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from catalog.models import Product
 
 
 def home(request):
@@ -7,3 +8,17 @@ def home(request):
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+def oun_product(request, pk):
+    """Контролер одного товара"""
+    prod = Product.objects.get(pk=pk)
+    context = {"prod": prod}
+    return render(request, 'oun_product.html', context)
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"product": products}
+    return render(request, 'product_list.html', context)
+
