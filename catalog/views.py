85e9f0d49c4351bc74f58f065024750558834910
Aleksandr <feat9999@yandex.ru>
2025-07-07 14:03:05 +0400

    commit updates

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
+import logging
 
 
 def home(request):
@@ -12,14 +13,14 @@ def contacts(request):
 
 def oun_product(request, pk):
     """Контролер одного товара"""
-    prod = Product.objects.get(pk=pk)
+    prod = get_object_or_404(Product, pk=pk)
     context = {"prod": prod}
     return render(request, 'oun_product.html', context)
 
 
 def products_list(request):
     """Контролер главная"""
-    products = Product.objects.all()
-    context = {"product": products}
+    product = Product.objects.all()
+    context = {"product": product}
     return render(request, 'product_list.html', context)
 
